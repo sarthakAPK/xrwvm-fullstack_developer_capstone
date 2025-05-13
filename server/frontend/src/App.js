915aa3a230ffc2b49344396fd9737ef8397ec9db
sarthakAPK <sarthakkarekar182005@gmail.com>
2025-05-13 16:11:41 -0400

    Added registration page with React component and route

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import RegisterPanel from "./components/Register/Register"; // Add this import
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<RegisterPanel />} /> {/* Add this line */}
     </Routes>
   );
 }
+
 export default App;
